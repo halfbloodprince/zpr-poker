5cb56718caa38c2da578871270684b67c0465af1
Severus <pfpawelec@gmail.com>
2014-12-13 15:50:39 +0100

    Add simple design of request and handler mechanism
    Request is command pattern, probably will be created by factory.
    
    Signed-off-by: Severus <pfpawelec@gmail.com>

---
+++ src/server/request_handler.hpp
@@ -0,0 +1,15 @@
+#ifndef POKER_REQUEST_HANDLER_H
+#define POKER_REQUEST_HANDLER_H
+
+#include "server/request.hpp"
+
+/// @brief Base class for all handlers
+class RequestHandler
+{
+public:
+	/// @brief Handle given request
+	/// @param[in] req Request object to be handled
+	virtual void handle(Request *req) = 0;
+};
+
+#endif
